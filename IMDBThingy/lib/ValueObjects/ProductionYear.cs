26122a49f4dedfaba20a9378a6dac6b00e90925e
Zexuz <Robin.edbom@gmail.com>
2016-10-08 19:59:28 +0200

    It's working.. For now

---
+++ IMDBThingy/lib/ValueObjects/ProductionYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IMDBThingy.lib.ValueObjects {
+
+    public class ProductionYear {
+
+        public int Year { get; }
+
+        public ProductionYear(int year) {
+            if(year > DateTime.Now.Year || year< 1800) throw new Exception("I have no time to implement a new exception for this, but the year is invalid");
+            Year = year;
+            Year = year;
+        }
+
+        public override string ToString() {
+            return Year.ToString();
+        }
+
+    }
+
+}
