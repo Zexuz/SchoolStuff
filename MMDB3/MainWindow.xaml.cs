ecb3fd77fe86752ea58cf7e82ba792cfa6456cc3
Zexuz <Robin.edbom@gmail.com>
2016-10-22 17:17:21 +0200

    Just a save before making averyting bad

---
+++ MMDB3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MMDB.MovieDatabase.Services;
+
+
+namespace MMDB3 {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private readonly FreeTextSearchService _searchService;
+
+        public MainWindow() {
+            InitializeComponent();
+            _searchService = new FreeTextSearchService();
+        }
+
+
+        private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var searchText = SearchTextBox.Text;
+            var everybody = _searchService.Search(searchText, true);
+            MovieList.ItemsSource = everybody;
+        }
+
+
+    }
+
+}
---
+++ MMDB3/SearchResultConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+using MMDB.MovieDatabase.Domain;
+using MMDB.MovieDatabase.Repositories;
+using MMDB.MovieDatabase.ValueObjects;
+
+using MMDB3.MovieDatabase.Domain.Entities;
+
+
+namespace MMDB3 {
+
+    public class SearchResultConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value == null) return null;
+
+
+            if (value.GetType() == typeof(MovieSearchResultItem)) {
+                var item = (Movie) ((MovieSearchResultItem) value).ResultItem;
+
+                switch ((string) parameter) {
+                    case "Name":
+                        return item.Title;
+                    case "Directors":
+                        return string.Join("\n",
+                            item.DirectorIds.Select(
+                                directorId => CastOrCrewRepository.Instance.FindBy(directorId).Name).ToList()
+                        );
+                    case "Actors":
+                        return string.Join("\n",
+                            item.ActorIds.Select(
+                                actorIds => CastOrCrewRepository.Instance.FindBy(actorIds).Name).ToList()
+                        );
+                    default:
+                        return item;
+                }
+            }
+
+            if (value.GetType() == typeof(CastOrCrewSearchResultItem)) {
+                var cast = (CastOrCrew) ((CastOrCrewSearchResultItem) value).ResultItem;
+
+                switch ((string) parameter) {
+                    case "Name":
+                        return cast.Name;
+                    case "Directors":
+                        return string.Join("\n", (from movieId in cast.DirectedMovieIds
+                            from movie in MovieRepository.Instance.movies
+                            where movie.Id == movieId
+                            select $"{movie.Title}({movie.ProductionYear})").ToList());
+                    case "Actors":
+                        return string.Join("\n", (from movieId in cast.ActedMovieIds
+                            from movie in MovieRepository.Instance.movies
+                            where movie.Id == movieId
+                            select $"{movie.Title}({movie.ProductionYear})").ToList());
+                    default:
+                        return cast;
+                }
+            }
+
+            throw new Exception($"Cannot convert from type {value.GetType().ToString()}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
